af0baecad7820813ab9519d330846358321c67ce
huutoan <huutrantoan@gmail.com>
2022-02-25 22:46:05 +0700

    Categories

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,31 +1,10 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  position: relative;
-`;
-
-const Arrow = styled.div`
-  width: 50px;
-  height: 50px;
-  background-color: #fff7f7;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
-  cursor: pointer;
-  opacity: 0.8;
-  z-index: 1;
+  flex: 1;
 `;
 
 const Wrapper = styled.div`
@@ -42,28 +21,35 @@ const ImgContainer = styled.div`
 `;
 
 const Img = styled.img`
-  width: 100vw;
+  width: 100%;
   height: auto;
-  max-width: 100%;
-  max-height: 100%;
+  object-fit: cover;
 `;
 
 const InfoContainer = styled.div`
   color: white;
   position: absolute;
-  top: 40%;
-  left: 35%;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h1`
   font-size: 60px;
   font-style: italic;
+  text-align: right;
 `;
 const Description = styled.p`
   font-size: 40px;
   letter-spacing: 0.2em;
   padding-left: 190px;
   transform: translateX(0.1em);
+  text-align: right;
 `;
 const Button = styled.button`
   cursor: pointer;
@@ -72,6 +58,8 @@ const Button = styled.button`
   border-radius: 4px;
   padding: 12px 20px;
   border: 1px solid transparent;
+  justify-content: between;
+  margin: 10px 45px;
   &:hover {
     background-color: black;
     color: white;
@@ -81,20 +69,18 @@ const Button = styled.button`
 `;
 
 const CardButton = styled.div`
+  width: 50%;
   display: flex;
   margin-top: 40px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
   outline: none !important;
+  justify-content: center;
 `;
 
 const Slider = () => {
   return (
     <Container>
-      <Arrow direction="left">
-        <ArrowLeftOutlined />
-      </Arrow>
       <Wrapper>
         <Slide>
           <ImgContainer>
@@ -107,15 +93,29 @@ const Slider = () => {
             <Title>NEW COLLECTION</Title>
             <Description>SPRING 2022</Description>
             <CardButton>
-              <Button>NEW MEN'S ITEMS</Button>
               <Button>NEW WOMEN'S ITEMS</Button>
+              <Button>NEW MEN'S ITEMS</Button>
             </CardButton>
           </InfoContainer>
         </Slide>
+        {/* <Slide>
+          <ImgContainer>
+            <Img
+              src="https://www.ovsfashion.com/on/demandware.static/-/Sites/default/dwbb86c778/08022022/HP-SALDI-DSK-TAGLI_03_1920.d809a3a1.webp"
+              alt=""
+            />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SPRING FUN</Title>
+            <Description>NEW KIDS</Description>
+            <Description>COLLECTION</Description>
+            <CardButton>
+              <Button>TEEN GIRLS 10-15 YEARS</Button>
+              <Button>TEEN BOYS 10-15 YEARS</Button>
+            </CardButton>
+          </InfoContainer>
+        </Slide> */}
       </Wrapper>
-      <Arrow direction="right">
-        <ArrowRightOutlined />
-      </Arrow>
     </Container>
   );
 };
